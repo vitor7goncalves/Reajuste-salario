9fa5ce3dea8489ba2f3a98c8673f4cbb86939a81
Vitor Gonçalves Rodrigues <vitor7goncalves@outlook.com>
2021-02-23 19:27:58 -0300

    Reajustando os if()

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ namespace teste_reajuste_salario
                     Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
                     Console.WriteLine("Em percentual: {0} %", percentual);
                 }
-                else if (salario > 400.01 && salario <= 800.00)
+                else if (salario > 400.00 && salario <= 800.00)
                 {
                     reajuste = salario * 0.12;
                     novoSalario = salario + reajuste;
@@ -35,7 +35,7 @@ namespace teste_reajuste_salario
                     Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
                     Console.WriteLine("Em percentual: {0} %", percentual);
                 }
-                else if (salario > 800.01 && salario <= 1200.00)
+                else if (salario > 800.00 && salario <= 1200.00)
                 {
                     reajuste = salario * 0.10;
                     novoSalario = salario + reajuste;
@@ -44,7 +44,7 @@ namespace teste_reajuste_salario
                     Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
                     Console.WriteLine("Em percentual: {0} %", percentual);
                 }
-                else if (salario > 1200.01 && salario <= 2000.00)
+                else if (salario > 1200.00 && salario <= 2000.00)
                 {
                     reajuste = salario * 0.07;
                     novoSalario = salario + reajuste;
